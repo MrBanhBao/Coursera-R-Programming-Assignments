ef0ae4aee4b6e3c7ed2c124a5b8d36205afb6bdd
Hao Nguyen <s0546568@htw-berlin.de>
2016-05-29 09:49:21 +0200

    finished second assignment

--- Assignment2/cachematrix.R
+++ Assignment2/cachematrix.R
@@ -4,12 +4,37 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      i <- NULL
+      set <- function(y) {
+            #operator to assign a value to an object in an environment that is different than the current environment
+            x <<- y 
+            i <<- NULL
+      }
+      
+      get <- function() x
+      
+      setInverse <- function(inverse) i <<- inverse
+      
+      getInverse <- function() i
+      
+      #map functions into a list
+      list(set=set, get=get, 
+           setInverse=setInverse, getInverse=getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      i <- x$getInverse()
+      if(!is.null(i)) {
+            message("Getting cached Data.")
+            return(i)
+      }
+      
+      data <- x$get()
+      i <- solve(data)
+      x$setInverse(i)
+      i
 }
